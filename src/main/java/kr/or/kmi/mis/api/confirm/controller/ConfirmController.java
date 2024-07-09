0d122c690199293e173ca372968f4c4bb8306e11
Hyeyeon <khy33355@gmail.com>
2024-07-09 14:15:05 +0900

    REFACTOR: Confirm 코드 수정 및 ServiceImpl 분리

--- src/main/java/kr/or/kmi/mis/api/confirm/controller/ConfirmController.java
+++ src/main/java/kr/or/kmi/mis/api/confirm/controller/ConfirmController.java
@@ -2,39 +2,35 @@ package kr.or.kmi.mis.api.confirm.controller;
 
 import kr.or.kmi.mis.api.bcd.model.response.BcdDetailResponseDTO;
 import kr.or.kmi.mis.api.confirm.service.ConfirmService;
+import kr.or.kmi.mis.cmm.response.ApiResponse;
+import kr.or.kmi.mis.cmm.response.ResponseWrapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/confirm")
+@RequestMapping("api/bsc/applyList")
 public class ConfirmController {
 
     private ConfirmService confirmService;
 
     /* 신청 목록 상세 조회 */
-    @GetMapping("applyList/{draftId}")
-    public ResponseEntity<BcdDetailResponseDTO> getApplyList(@RequestParam("draftId") Long id) {
-        BcdDetailResponseDTO bcdDetail = confirmService.getBcdDetailInfo(id);
-        return ResponseEntity.status(HttpStatus.OK).body(bcdDetail);
+    @GetMapping("/{draftId}")
+    public ApiResponse<BcdDetailResponseDTO> getApplyList(@PathVariable Long draftId) {
+        return ResponseWrapper.success(confirmService.getBcdDetailInfo(draftId));
     }
 
-//    /* 승인 */
-//    @PostMapping("applyList/{draftId}}")
-//    public ResponseEntity<String> approve(@RequestParam("draftId") Long id) {
-//        confirmService.approve(id);
-//        return new ResponseEntity<>("ok", HttpStatus.OK);
-//    }
-//
-//    /* 반려 */
-//    @PostMapping("applyList/return/{draftId}")
-//    public ResponseEntity<String> reject(@RequestParam("draftId") Long id, @RequestBody String rejectReason) {
-//        confirmService.reject(id, rejectReason);
-//        return new ResponseEntity<>("ok", HttpStatus.OK);
-//    }
+    /* 승인 */
+    @PostMapping("/{draftId}")
+    public ApiResponse<?> approve(@PathVariable Long draftId) {
+        confirmService.approve(draftId);
+        return ResponseWrapper.success();
+    }
+
+    /* 반려 */
+    @PostMapping("/return/{draftId}")
+    public ApiResponse<?> disapprove(@PathVariable Long draftId, @RequestBody String rejectReason) {
+        confirmService.disapprove(draftId, rejectReason);
+        return ResponseWrapper.success();
+    }
 }
