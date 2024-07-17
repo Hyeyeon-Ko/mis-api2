3c2ab8fe7e8199c5247044560978428559155e11
clsung <clsung@kmi.or.kr>
2024-07-17 13:19:06 +0900

    QueryDSL build.gradle 추가 및 샘플소스

---
+++ src/main/java/kr/or/kmi/mis/api/std/repository/StdClassQueryRepository.java
@@ -0,0 +1,16 @@
+package kr.or.kmi.mis.api.std.repository;
+
+/**
+ * packageName    : kr.or.kmi.mis.api.std.repository
+ * fileName       : StdClassQueryRepository
+ * author         : KMI_DI
+ * date           : 2024-07-17
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-07-17        KMI_DI       the first create
+ */
+public interface StdClassQueryRepository {
+    String sample(String code);
+}
