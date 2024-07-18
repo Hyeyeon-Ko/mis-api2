2416060c9d784dd347cf96dbd32599b58e189366
clsung <clsung@kmi.or.kr>
2024-07-18 15:02:13 +0900

    오류처리 관련 로직 수정

--- src/main/java/kr/or/kmi/mis/cmm/controller/GlobalExceptionHandler.java
+++ src/main/java/kr/or/kmi/mis/cmm/controller/GlobalExceptionHandler.java
@@ -3,24 +3,28 @@ package kr.or.kmi.mis.cmm.controller;
 import kr.or.kmi.mis.api.exception.EntityNotFoundException;
 import kr.or.kmi.mis.cmm.response.ApiResponse;
 import kr.or.kmi.mis.cmm.response.ResponseWrapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+
     @ExceptionHandler(EntityNotFoundException.class)
-    public ApiResponse<?> EntityNotFoundException(final EntityNotFoundException e) {
-        return ResponseWrapper.error(e.getMessage());
+    public ResponseEntity<ApiResponse<String>> handleEntityNotFoundException(EntityNotFoundException e) {
+        return new ResponseEntity<>(ResponseWrapper.error(e.getMessage()), HttpStatus.NOT_FOUND);
     }
 
+
     @ExceptionHandler(IllegalArgumentException.class)
-    public ApiResponse<?> IllegalArgumentException(final IllegalArgumentException e) {
-        return ResponseWrapper.error();
+    public ResponseEntity<ApiResponse<String>> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<>(ResponseWrapper.error(e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NullPointerException.class)
-    public ApiResponse<?> NullPointerException(final NullPointerException e) {
-        return ResponseWrapper.error();
+    public ResponseEntity<ApiResponse<String>> handleNullPointerException(NullPointerException e) {
+        return new ResponseEntity<>(ResponseWrapper.error(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
