02ef600bd8af898a69c6973120a1a96ef3e8ba37
sungah <sungah0815@gmail.com>
2024-09-13 14:15:35 +0900

    ETC: ..

--- src/main/java/kr/or/kmi/mis/api/order/service/Impl/OrderServiceImpl.java
+++ src/main/java/kr/or/kmi/mis/api/order/service/Impl/OrderServiceImpl.java
@@ -7,9 +7,8 @@ import kr.or.kmi.mis.api.bcd.model.entity.BcdMaster;
 import kr.or.kmi.mis.api.bcd.repository.BcdDetailRepository;
 import kr.or.kmi.mis.api.bcd.repository.BcdMasterRepository;
 import kr.or.kmi.mis.api.exception.EntityNotFoundException;
-import kr.or.kmi.mis.api.noti.model.response.SseResponseDTO;
-import kr.or.kmi.mis.api.noti.service.NotificationService;
-import kr.or.kmi.mis.api.order.model.request.OrderRequestDTO;
+import kr.or.kmi.mis.api.noti.service.NotificationSendService;
+\import kr.or.kmi.mis.api.order.model.request.OrderRequestDTO;
 import kr.or.kmi.mis.api.order.model.response.EmailSettingsResponseDTO;
 import kr.or.kmi.mis.api.order.model.response.OrderListResponseDTO;
 import kr.or.kmi.mis.api.order.service.ExcelService;
@@ -46,7 +45,7 @@ public class OrderServiceImpl implements OrderService {
     private final StdDetailRepository stdDetailRepository;
     private final ExcelService excelService;
     private final StdBcdService stdBcdService;
-    private final NotificationService notificationService;
+    private final NotificationSendService notificationSendService;
 
     @Override
     @Transactional(readOnly = true)
@@ -116,15 +115,16 @@ public class OrderServiceImpl implements OrderService {
             // 알림 전송
             BcdDetail bcdDetail = bcdDetailRepository.findById(draftId)
                     .orElseThrow(() -> new EntityNotFoundException("BcdDetail not found with id " + draftId));
+            notificationSendService.sendBcdOrder(bcdMaster.getDraftDate(), bcdDetail.getUserId());
 
             String type = "BCD";
             String now = simpleDateTimeFormat.format(new Timestamp(System.currentTimeMillis()));
             String content = "[수령확인] " + simpleDataFormat.format(bcdMaster.getDraftDate())
                     + " 신청한 명함이 [발주요청] 되었습니다./수령하신 후, 수령확인 버튼을 눌러주세요.";
 
-            SseResponseDTO sseResponseDTO = SseResponseDTO.of(bcdMaster.getDraftId(), content, type, now);
-            Long userId = Long.parseLong(bcdDetail.getUserId());
-            notificationService.customNotify(userId, sseResponseDTO, "명함신청 수령안내");
+//            SseResponseDTO sseResponseDTO = SseResponseDTO.of(bcdMaster.getDraftId(), content, type, now);
+//            Long userId = Long.parseLong(bcdDetail.getUserId());
+//            notificationService.customNotify(userId, sseResponseDTO, "명함신청 수령안내");
 
         });
     }
