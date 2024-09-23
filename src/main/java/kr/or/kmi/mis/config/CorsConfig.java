4a0b2c8f9bc2f9ae338dfcbd5b3fe1a39b56e7ec
clsung <clsung@kmi.or.kr>
2024-09-24 07:42:04 +0900

    개발서버 운영서버 로컬서버 cors 추가

--- src/main/java/kr/or/kmi/mis/config/CorsConfig.java
+++ src/main/java/kr/or/kmi/mis/config/CorsConfig.java
@@ -23,10 +23,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-//                        .allowedOriginPatterns("http://localhost:3000")  // 로컬주소(개발진행시)
-//                        .allowedOriginPatterns("http://xxx.xxx.xxx")  // 운영서버 주소
-//                        .allowedOriginPatterns("http://101.10.1.87:3000")  // 개발서버 주소
-                        .allowedOriginPatterns("http://101.10.1.87:3000", "http://localhost:3000")
+                        .allowedOriginPatterns("http://localhost:3000", "http://101.10.1.87:3000", "http://172.16.250.87")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
                         .exposedHeaders("Authorization");
